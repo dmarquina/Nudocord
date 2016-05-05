5eb6d898e341fceb4f3cc7bdb516c3672d31f987
dmarquina <mrdiego0892@gmail.com>
2016-05-05 00:44:20 -0500

    signin and singup finished

--- clients/views.py
+++ clients/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+from .models import Client
 
 
 def signin(request):
@@ -28,11 +29,13 @@ def signup(request):
 			action = request.POST.get('action', None)
 			email = request.POST.get('email', None)
 			password = request.POST.get('password', None)
+			name = request.POST.get('name', None)
 
 			if action == 'signup':
-				user = User.objects.create_user(email=email,
-	                                            password=password)
-				user.save()
+				client = Client.objects.create_user(email=email,
+	                                            password=password,
+	                                            name=name,)
+				client.save()
 				return redirect('/signin')
 	context = {}
 	return render(request, 'signin/signup.html', context)

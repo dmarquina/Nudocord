2c66874a17b49df89a9f734be947301c06b3c699
dmarquina <mrdiego0892@gmail.com>
2016-05-24 16:12:45 -0500

    userprofile added, client independent

--- carts/urls.py
+++ carts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^/(?P<pk>[0-9]+)$', views.addtocart, name='addtocart'),
+	url(r'^(?P<pk>[0-9]+)$', views.addtocart, name='addtocart'),
 	url(r'^orders/cart_list/$', views.cartdetail, name='cartdetail'),
 ]
